e2498bb20bfe6ae33ffc704c8ce5239fb72801c4
Alex Villanova <alexsrvillanova@gmail.com>
2019-04-15 02:21:15 -0500

    Entity

--- src/main/java/usuarios/entity/Usuario.java
+++ src/main/java/usuarios/entity/Usuario.java
@@ -6,10 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.br.CPF;
 
 @Entity
+@Table(name="usuarios")
 public class Usuario {
 	@Id
     @GeneratedValue(strategy= GenerationType.AUTO)
