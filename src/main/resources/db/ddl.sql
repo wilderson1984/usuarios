2ac3c493dcf43497ed38863799a9688fef1d771f
Alex Villanova <alexsrvillanova@gmail.com>
2019-04-15 03:04:33 -0500

    Entity

--- src/main/resources/db/ddl.sql
+++
@@ -1,5 +0,0 @@
-drop table if exists USUARIOS
-drop table if exists hibernate_sequence
-create table USUARIOS (ID int not null, primary key (ID), CPF int, NAME varchar(255), EMAIL varchar(255), PROFISSAO varchar(255), GENERO varchar(255), ENDERECO varchar(255), DT_NASC date) engine=MyISAM
-create table hibernate_sequence (next_val bigint) engine=MyISAM
-insert into hibernate_sequence values ( 1 )
